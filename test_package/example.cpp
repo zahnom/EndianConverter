d8b084b669e65fcf11a26b66cd361297f35783f7
Marc Zahno <zahnom@me.com>
2020-08-04 18:55:32 +0200

    Added Conan package test

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-#include "hello.h"
+
+extern "C" {
+    #include "EndianConverter.h"
+}
 
 int main() {
-    hello();
+    uint32_t integer = 123456789;
+    uint8_t array[4] = {0};
+
+    BigEndian_UnsignedInt32ToArray(integer, array);
+    uint32_t result = BigEndian_ArrayToUnsignedInt32(array);
+
+    if(result == integer) {
+        printf("Package has been successfully installed!\n");
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_FAILURE;
 }
